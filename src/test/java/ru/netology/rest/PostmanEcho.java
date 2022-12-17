3861dd03dc487ac1495323f7adcba3320723b654
JaneEyre333 <nasgur@mail.ru>
2022-12-17 20:44:24 +0300

    PostmanEcho

---
+++ src/test/java/ru/netology/rest/PostmanEcho.java
@@ -0,0 +1,23 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class PostmanEcho {
+    @Test
+    void shouldReturnDemoAccounts() {
+    given()
+  .baseUri("https://postman-echo.com")
+  .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+.when()
+  .post("/post")
+// Проверки
+.then()
+  .statusCode(200)
+            .header("content-length", "425")
+            .header("Connection", "keep-alive")
+            .body("data", equalTo("some data"));
+}}
